1b1a07ec60ad37e9015fdae315c20da9682da413
tmharrison93 <tue74192@temple.edu>
2015-11-13 15:00:07 -0500

    working on adding customer class

---
+++ CIS3342TermProjectFall2015/TermProjectClassLib/Customer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TermProjectClassLib
+{
+    class Customer
+    {
+        string firstName;
+        string lastName;
+        string email;
+        string password;
+        string userType;
+
+        string shipAddress1;
+        string shipAddress2;
+        string shipCity;
+        string shipState;
+        int shipZip;
+
+        string billAddress1;
+        string billAddress2;
+        string billCity;
+        string cillState;
+        int billZip;
+
+        float totalDollarSales;     
+
+
+        public Customer()
+        {
+        }
+        public Customer(string fName, string lName, string email, string password, string userType)
+        {
+            this.firstName = fName;
+            this.lastName = lName;
+            this.email = email;
+            this.password = password;
+            this.userType = userType;
+        }
+
+        public void setShippingAddress(string add1, string add2, string city, string state, int zip)
+        {
+            shipAddress1 = add1;
+            shipAddress2 = add2;
+            shipCity = city;
+            shipState = state;
+            shipZip = zip;
+        }
+
+        public void setBillingAddress(string add1, string add2, string city, string state, int zip)
+        {
+
+        }
+    }
+}
