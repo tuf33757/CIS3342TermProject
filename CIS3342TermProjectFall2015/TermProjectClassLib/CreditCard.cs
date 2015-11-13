386522d1153fb2efd5e72e9a50a803da4df7e0b9
tmharrison93 <tue74192@temple.edu>
2015-11-13 15:34:45 -0500

    Added Old WS Project

---
+++ CIS3342TermProjectFall2015/TermProjectClassLib/CreditCard.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TermProjectClassLib
+{
+    class CreditCard
+    {
+        private String cardNumber;
+        private String cardType;
+        private int expirationMonth;
+        private int expirationYear;
+
+
+        public void setCardNum(string number)
+        {
+            cardNumber = number;
+        }
+
+        public void setCardType(string type)
+        {
+            cardType = type;
+        }
+
+        public void setExpMonth(int month)
+        {
+            expirationMonth = month;
+        }
+
+        public void setExpYear(int year)
+        {
+            expirationYear = year;
+        }
+
+        public String getCardNum()
+        {
+            return cardNumber;
+        }
+
+        public String getCardType()
+        {
+            return cardType;
+        }
+
+        public int getExpMonth()
+        {
+            return expirationMonth;
+        }
+
+        public int getExpYear()
+        {
+            return expirationYear;
+        }
+    }
+}
