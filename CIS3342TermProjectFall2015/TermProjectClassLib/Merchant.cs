ed2d281be4f9ddd852495fa53d3de1f98f5c5838
tmharrison93 <teu74192@temple.edu>
2015-11-14 11:29:11 -0500

    Added merchant class

---
+++ CIS3342TermProjectFall2015/TermProjectClassLib/Merchant.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TermProjectClassLib
+{
+    class Merchant
+    {
+        string merchantID;
+        public string storeName;
+        public string firstName;
+        public string lastName;
+        string APIKey;
+        public string storeDescrip;
+
+        public Merchant()
+        {
+        }
+
+        public Merchant(string ID, string storename, string fname, string lname, string api, string login, string descrip)
+        {
+            merchantID = ID;
+            storeName = storename;
+            firstName = fname;
+            lastName = lname;
+            APIKey = api;
+            storeDescrip = descrip;
+        }
+
+       public string getMerchantID()
+       {
+           return merchantID;
+       }
+       public string getAPIKey()
+       {
+           return APIKey;
+       }
+
+        //WRITE THE LOGIC TO GENERATE AN API KEY HERE
+    }
+}
