a020f617fd1d9535af39bb72395d4aa8161f3998
tuf33757 <tuf33757@temple.edu>
2015-11-14 15:29:53 -0500

    merch reg

--- CIS3342TermProjectFall2015/CIS3342TermProjectFall2015/TP_Merchant_Registration.aspx.cs
+++ CIS3342TermProjectFall2015/CIS3342TermProjectFall2015/TP_Merchant_Registration.aspx.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using TermProjectClassLib;
+
 
 namespace CIS3342TermProjectFall2015
 {
     public partial class TP_Merchant_Registration : System.Web.UI.Page
     {
+        Merchant merch = new Merchant();
         protected void Page_Load(object sender, EventArgs e)
         {
             //string session = (string)Session["Login"];
@@ -18,8 +21,8 @@ namespace CIS3342TermProjectFall2015
 
         protected void btnSubmitMerch_Click(object sender, EventArgs e)
         {
-            if (checkForNull(this))
-            { }
+            if (checkForNull()== true)
+            { lblInform.Text = "Cogratz you win!"; }
             else
                 lblInform.Text = "Please enter all required fields.";
 
@@ -50,18 +53,26 @@ namespace CIS3342TermProjectFall2015
                 }
             }
         }
-
-        protected Boolean checkForNull(Control page)
+        protected Boolean checkForNull()
         {
-            foreach (Control c in page.Controls)
-            {
-                if ((c is TextBox) && (c as TextBox).Text == "")
-                    return false;
-                if ((c is DropDownList) && (c as DropDownList).SelectedIndex == 0)
-                    return false;
-            }
-            return true;
+            if ((txtFirstName.Text == "") || (txtLastName.Text == "") || (txtEmail.Text == "")
+                || (txtLoginId.Text == "") || (txtStoreDescription.Text == "") || (txtStoreName.Text == ""))
+                return false;
+            else
+                return true;
         }
+        //protected Boolean checkForNull(Control page)
+        //{
+        //    foreach (Control c in page.Controls)
+        //    {
+        //        if (c is TextBox){
+        //            if((c as TextBox).Text == "")
+        //            return false;}
+        //        if ((c is DropDownList) && (c as DropDownList).SelectedIndex == 0)
+        //            return false;
+        //    }
+        //    return true;
+        //}
 
     }
 }
