ab29b2dfb9bf49bdebd41f0ba8417bdaa9e86dc5
tmharrison93 <tue74192@temple.edu>
2015-12-01 13:38:37 -0500

    Began Shopping Cart Serialization

---
+++ CIS3342TermProjectFall2015/TermProjectClassLib/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TermProjectClassLib
+{
+    class Product
+    {
+        public int productNum;
+        public int productPrice;
+        string prodDescript;
+        int QOH;
+        string departmentNum;
+        string url = "";
+
+        public Product()
+        {
+        }
+
+        public Product(int productNum, int productPrice, string prodDescript, int QOH, string departmentNum, string url)
+        {
+            this.productNum = productNum;
+            this.productPrice = productPrice;
+            this.prodDescript = prodDescript;
+            this.QOH = QOH;
+            this.departmentNum = departmentNum;
+            this.url = url;
+        }
+    }
+}
---
+++ CIS3342TermProjectFall2015/TermProjectClassLib/ShoppingCart.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TermProjectClassLib
+{
+    [Serializable]
+    class ShoppingCart
+    {
+        public Product[] CartItems;
+        public string cartID;
+        public string customerID;
+
+        public ShoppingCart()
+        {
+        }
+
+        public ShoppingCart(Product[] CartItems, string customerID)
+        {
+            this.CartItems = CartItems;
+            this.customerID = customerID;
+        }
+
+    }
+}
