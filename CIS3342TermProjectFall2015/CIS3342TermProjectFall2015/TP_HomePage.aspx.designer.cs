318c4cb8770a043513bdeeda86a06207936a2b11
tmharrison93 <tue74192@temple.edu>
2015-12-04 15:07:52 -0500

    SERIALIZATION WORKS

--- CIS3342TermProjectFall2015/CIS3342TermProjectFall2015/TP_HomePage.aspx.designer.cs
+++ CIS3342TermProjectFall2015/CIS3342TermProjectFall2015/TP_HomePage.aspx.designer.cs
@@ -94,6 +94,15 @@ namespace CIS3342TermProjectFall2015 {
         protected global::System.Web.UI.WebControls.GridView gvCatalog;
         
         /// <summary>
+        /// btnPurchase control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnPurchase;
+        
+        /// <summary>
         /// custInfo control.
         /// </summary>
         /// <remarks>
