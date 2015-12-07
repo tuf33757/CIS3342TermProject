f8fc7bd595926b68bed5d9574c20ce41e08f335c
Rob Zahorchak <tuf33757@temple.edu>
2015-12-07 11:23:56 -0500

    fixed the dd error

--- CIS3342TermProjectFall2015/CIS3342TermProjectFall2015/TP_Customer_Accnt.aspx.designer.cs
+++ CIS3342TermProjectFall2015/CIS3342TermProjectFall2015/TP_Customer_Accnt.aspx.designer.cs
@@ -103,15 +103,6 @@ namespace CIS3342TermProjectFall2015 {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl shoppingInfo;
         
         /// <summary>
-        /// ddDepartment control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.DropDownList ddDepartment;
-        
-        /// <summary>
         /// gvAccount control.
         /// </summary>
         /// <remarks>
